19fe3b63012c4ca843403fe7cc736f49b1a9b471
ashgw <ashrefgw@proton.me>
2025-03-05 15:56:06 +0100

    test: remove `istanbul` coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,17 +5,5 @@ export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
     includeSource: ['src/**/[!index]*.ts'],
-    coverage: {
-      provider: 'istanbul',
-      reporter: ['lcov', 'text', 'json', 'html'],
-      all: true,
-      include: ['src/**/*'],
-      watermarks: {
-        lines: [80, 95],
-        functions: [80, 95],
-        branches: [80, 95],
-        statements: [80, 95],
-      },
-    },
   },
 });
